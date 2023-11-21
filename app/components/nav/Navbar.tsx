d5ce13cc696c477dd1d43da02adeacf95269fbb4
wael <wael.22@live.fr>
2023-11-21 13:47:12 +0100

    my first commit

---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import Container from "../Container"
+import Image from "next/image"
+
+
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link href={"/"}>
+             <Image src={"/images/explores_word.png"} width={150} height={100} alt=""/>
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>User Menu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
