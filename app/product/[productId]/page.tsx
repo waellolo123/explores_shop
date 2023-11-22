5c92d6b982e42507b9b9e422d75c6745cf88f7cd
wael <wael.22@live.fr>
2023-11-22 18:15:47 +0100

    finishing single page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,8 @@
 import Container from "@/app/components/Container"
 import { product } from "@/utils/product"
 import ProductDetails from "./ProductDetails"
+import ListRating from "../ListRating"
+
 
 interface IParams {
   productId?: string
@@ -13,6 +15,10 @@ const Product = ({params} : {params:IParams}) => {
     <div className="p-8">
      <Container>
       <ProductDetails product={product}/>
+      <div className="flex flex-col mt-20 gap-4">
+        <div className="">Add Rating</div>
+        <ListRating product={product} />
+      </div>
      </Container>
     </div>
   )
--- utils/product.ts
+++ utils/product.ts
@@ -1,24 +1,46 @@
-export const product = {
-    id: "64a654593e91b8e73a351e9b",
-    name: "Flyer",
-    description: "Windproof ultralight and breathable fabric resist liquid water passing through, but allow water vapour to pass through. The ability to block out rain for a short time is very powerfull. A cilicon coating will keep you dry because the water fall down in the blink of an eye. ",
-    price: 69,
-    brand: "bigK",
-    category: "Flyer",
-    inStock: true,
-    images: [
-      {
-        color: "White",
-        colorCode: "#FFFFFF",
+export const product =   {
+  id: "64a4ebe300900d44bb50628a",
+  name: "Frigate",
+  description:
+    "PERFECT STROKE KEYS - Spherically-dished keys match the shape of your fingertips, offering satisfying feedback with every tap\nCOMFORT AND STABILITY - Type with confidence on a keyboard crafted for comfort, stability, and precision",
+  price: 39.99,
+  brand: "logitech",
+  category: "Accesories",
+  inStock: true,
+  images: [
+    {
+      color: "Black",
+      colorCode: "#000000",
+      image:
+        "https://bigksport.com/cdn/shop/products/IMG_8816.jpg?v=1688030683&width=360",
+    },
+    {
+      color: "Red",
+      colorCode: "#dd0f1a",
+      image:
+        "https://bigksport.com/cdn/shop/products/IMG_8820.jpg?v=1684706079&width=713",
+    },
+  ],
+  reviews: [
+    {
+      id: "64a65a6158b470c6e06959ee",
+      userId: "6475af156bad4917456e6e1e",
+      productId: "64a4ebe300900d44bb50628a",
+      rating: 5,
+      comment: "good",
+      createdDate: "2023-07-06T06:08:33.067Z",
+      user: {
+        id: "6475af156bad4917456e6e1e",
+        name: "Charles",
+        email: "example@gmail.com",
+        emailVerified: null,
         image:
-          "https://bigksport.com/cdn/shop/products/IMG_2142_7f1b611b-73bb-40db-886c-0ef97f19f71a.png?v=1680249627&width=360",
+          "https://bigksport.com/cdn/shop/products/IMG_8855.jpg?v=1681462479&width=360",
+        hashedPassword: null,
+        createdAt: "2023-05-30T08:08:53.979Z",
+        updatedAt: "2023-05-30T08:08:53.979Z",
+        role: "ADMIN",
       },
-      {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-          "https://bigksport.com/cdn/shop/products/BAD0A6F2-F163-4EE4-9DB7-0B5E5B2C6F64.jpg?v=1684762295&width=360",
-      },
-    ],
-    reviews: [],
-  }
+    },
+  ],
+}
