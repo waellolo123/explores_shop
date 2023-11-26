9912ba2b5d087e081fe3cedfc41d65d45126e83b
wael <wael.22@live.fr>
2023-11-26 20:14:06 +0100

    add products page

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import CategoryInput from "@/app/components/inputs/CategoryInput";
 import CustomCheckbox from "@/app/components/inputs/CustomCheckbox";
 import Input from "@/app/components/inputs/Input";
+import SelectColor from "@/app/components/inputs/SelectColor";
 import TextArea from "@/app/components/inputs/TextArea";
-import { useState } from "react";
+import { categories } from "@/utils/Categories";
+import { colors } from "@/utils/Colors";
+import { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+export type ImageType = {
+  color: string;
+  colorCode: string;
+  image: File | null
+};
+
+export type UploadedImageType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
+
 const AddProductForm = () => {
+
  const [isLoading, setIsLoading] = useState(false);
+ const [images, setImages] = useState<ImageType[] | null>();
+ const [isProductCreated, setIsProductCreated] = useState(false);
  const {register, handleSubmit, setValue, watch, reset, formState:{errors}} = useForm<FieldValues>({
   defaultValues: {
     name: '',
@@ -20,6 +39,19 @@ const AddProductForm = () => {
   }, 
  })
 
+ useEffect(()=>{
+  setCustomValue('images', images)
+ },[images])
+
+ const category = watch('category');
+ const setConstantValue = (id: string, value: any) => {
+  setValue(id, value, {
+    shouldValidate: true,
+    shouldDirty: true,
+    shouldTouch: true,
+  });
+ }
+
   return (
     <>
      <Heading title="Add Product" />
@@ -30,8 +62,31 @@ const AddProductForm = () => {
      <CustomCheckbox id="inStock" register={register} label="This product is in stock" />
      <div className="w-full font-medium">
       <div className="mb-2 font-semibold">Select a category</div>
-      <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto">
-       
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-3 max-h-[50vh] overflow-y-auto">
+       {categories.map((item)=>{
+        if(item.label === 'All'){
+          return null;
+        }
+        return <div key={item.label} className="col-span">
+          <CategoryInput 
+          onclick={(category)=>setCustomValue('category', category)} 
+          selected={category === item.label}
+          label={item.label}
+          icon={item.icon}
+          />
+        </div>
+       })}
+      </div>
+     </div>
+     <div className="w-full flex flex-col flex-wrap gap-4">
+      <div className="">
+        <div className="font-bold">Select the available product sizes and upload their images.</div>
+        <div className="text-sm">You must upload an image for each of the size selected otherwise your color selection will be ignored.</div>
+      </div>
+      <div className="grid grid-cols-2 gap-3">
+        {colors.map((item, index)=>{
+          return (<SelectColor key={index} item={item} addImageToState={()=>{}} removeImageFromState={()=>{}} isProductCreated={false} />)
+        })}
       </div>
      </div>
     </>
@@ -39,4 +94,6 @@ const AddProductForm = () => {
 }
 
 export default AddProductForm
+
+
 
