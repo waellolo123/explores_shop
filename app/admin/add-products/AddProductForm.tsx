a079a20bacab02e3cca095cfaeadf31f8675e189
wael <wael.22@live.fr>
2023-11-26 14:45:23 +0100

    add product logic

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,8 +1,40 @@
 "use client";
 
+import Heading from "@/app/components/Heading";
+import CustomCheckbox from "@/app/components/inputs/CustomCheckbox";
+import Input from "@/app/components/inputs/Input";
+import TextArea from "@/app/components/inputs/TextArea";
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
 const AddProductForm = () => {
+ const [isLoading, setIsLoading] = useState(false);
+ const {register, handleSubmit, setValue, watch, reset, formState:{errors}} = useForm<FieldValues>({
+  defaultValues: {
+    name: '',
+    description: '',
+    brand: '',
+    inStock: false,
+    images: [],
+    price: '',
+  }, 
+ })
+
   return (
-    <div>AddProductForm</div>
+    <>
+     <Heading title="Add Product" />
+     <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
+     <Input id="price" label="Price" disabled={isLoading} type="number" register={register} errors={errors} required />
+     <Input id="brand" label="Brand" disabled={isLoading} register={register} errors={errors} required />
+     <TextArea id="description" label="Description" disabled={isLoading} register={register} errors={errors} required />
+     <CustomCheckbox id="inStock" register={register} label="This product is in stock" />
+     <div className="w-full font-medium">
+      <div className="mb-2 font-semibold">Select a category</div>
+      <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto">
+       
+      </div>
+     </div>
+    </>
   )
 }
 
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,9 +1,26 @@
+import Container from "@/app/components/Container"
+import FormWrapp from "@/app/components/FormWrapp"
+import AddProductForm from "./AddProductForm"
+import { getCurrentUser } from "@/actions/getCurrentUser"
+import NullData from "@/app/components/NullData"
 
 
-const AddProducts = () => {
+const AddProducts = async () => {
+ const currentUser = await getCurrentUser();
+ if(!currentUser || currentUser.role !== 'ADMIN'){
+  return <NullData title="Oops access denied!" />
+ }
+
   return (
-    <div>add products</div>
+    <div className="p-8">
+     <Container>
+      <FormWrapp>
+        <AddProductForm />
+      </FormWrapp>
+     </Container> 
+    </div>
   )
 }
 
 export default AddProducts
+
